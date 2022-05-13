53ae48b7c13cc17b50dea35c0c6cbb4c6bd1c323
2nd GenKev <59571011+alphacrutch@users.noreply.github.com>
2022-05-12 20:55:23 -0400

    Main UI Complete and Rules Modal Complete

---
+++ Scripts/game.js
@@ -0,0 +1,32 @@
+const paperDiv = document.getElementById("paperChoice");
+const rockDiv = document.getElementById("rockChoice");
+const scissorDiv = document.getElementById("scissorsChoice");
+const scoreDiv = document.getElementById("ScoreCounter");
+const score = 0;
+const overlay = document.getElementById("overlay");
+const rulesModal = document.getElementById("rulesModal");
+const closeRulesBtn = document.getElementById("closeRulesBtn");
+const RulesBtn = document.getElementById("rules");
+const ChoiceView = document.getElementById("ChoicesContainer");
+const WaitForAiView = document.getElementById("PickedView")
+const WinLooseView = document.getElementById("winLooseView");
+
+
+const ShowRules = () => {
+    rulesModal.style.display = "flex";
+    overlay.style.display = "flex";
+}
+const HideRules = () => {
+    rulesModal.style.display = "none";
+    overlay.style.display = "none";
+}
+const SetScore = () => {
+    score++;
+    scoreDiv.textContent = score;
+}
+paperDiv.addEventListener('click', SetScore);
+rockDiv.addEventListener('click', SetScore);
+scissorDiv.addEventListener('click', SetScore);
+closeRulesBtn.addEventListener('click', HideRules);
+overlay.addEventListener('click', HideRules);
+RulesBtn.addEventListener('click', ShowRules);
