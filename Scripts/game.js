0d7554c7535c2883f91f507139edd2966d72ffeb
2nd GenKev <59571011+alphacrutch@users.noreply.github.com>
2022-05-12 22:06:12 -0400

    Picked choice ui complete

--- Scripts/game.js
+++ Scripts/game.js
@@ -10,15 +10,31 @@ const RulesBtn = document.getElementById("rules");
 const ChoiceView = document.getElementById("ChoicesContainer");
 const WaitForAiView = document.getElementById("PickedView")
 const WinLooseView = document.getElementById("winLooseView");
+const myPicks = ['myPaper', 'myScissors', 'myRock'];
 var score = 0;
 
+var pickedElement;
 const ShowRules = () => {
     rulesModal.style.display = "flex";
     overlay.style.display = "flex";
 }
+const ShowMyPick = (elementID) => {
+    array.forEach(element => {
+        if (element == elementID) {
+            document.getElementById(element).style.display = "flex";
+        } else {
+            document.getElementById(element).style.display = "none";
+        }
+    });
+
+
+}
 const HideRules = () => {
     rulesModal.style.display = "none";
     overlay.style.display = "none";
+}
+const SetPickedChoice = () => {
+    pickedElement = e
 }
 const SetScore = () => {
     score++;
